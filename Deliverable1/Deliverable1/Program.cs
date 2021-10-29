44de6efca90228c8de26bd01e4c888b567716ba0
Aaliyah Kayum <90926318+Aali-K@users.noreply.github.com>
2021-10-29 13:30:54 -0400

    Add files via upload

---
+++ Deliverable1/Deliverable1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string calculation = "Y";
+            while (calculation == "Y")
+            {
+                Console.Write("input a measurement unit: ");
+                string unit = Console.ReadLine();
+
+                Console.Write("input an amount: ");
+                double amount = double.Parse(Console.ReadLine());
+
+                if (unit == "inch")
+                {
+                    amount *= 3.5;
+                    Console.WriteLine("Convert to " + amount + " fidget spinner(s).");
+                }
+                else if (unit == "Inch")
+                {
+                    amount *= 3.5;
+                    Console.WriteLine("Convert to " + amount + " fidget spinner(s).");
+                }
+                else if (unit == "Fidget Spinner")
+                {
+                    amount /= 3.5;
+                    Console.WriteLine("Convert to " + amount + "inch(es).");
+                }
+                else if (unit == "fidget spinner")
+                {
+                    amount /= 3.5;
+                    Console.WriteLine("Convert to " + amount + "inch(es).");
+                }
+                else if (unit == "Foot")
+                {
+                    amount *= 5;
+                    Console.WriteLine("Convert to " + amount + " meme(s).");
+                }
+                else if (unit == "foot")
+                {
+                    amount *= 5;
+                    Console.WriteLine("Convert to " + amount + " meme(s).");
+                }
+                else if (unit == "Meme")
+                {
+                    amount /= 5;
+                    Console.WriteLine("Convert to " + amount + " foot(feet).");
+                }
+                else if (unit == "meme")
+                {
+                    amount /= 5;
+                    Console.WriteLine("Convert to " + amount + " foot(feet).");
+                }
+                else
+                {
+
+                }
+                Console.Write("Do you wish to make another calculation? Please respond with Y or N");
+                string N = Console.ReadLine();
+                {
+                    if (unit == "N")
+                        Console.WriteLine("Good bye");
+                }
+            }
+        }
+    }
+}
+
